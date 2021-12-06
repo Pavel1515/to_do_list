efaaacc901aca67edf256e23095d6896978ceefc
Pavlo Yerebakan <ppvr3407@gmail.com>
2021-12-06 22:19:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let array = [];
+let x ;
+function  add(x) {
+    array[array.length] = x;
+};
+let y ;
+function clear(y) {
+    array.splice(y , 1);
+};
+let indicator  = array.length;
+function clear_all() {
+    array = [];
+}
+console.log(array);
