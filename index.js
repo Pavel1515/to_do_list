92258cd253cd7390f2dc7aedb5db1a0fa7fb24e9
Pavlo Yerebakan <ppvr3407@gmail.com>
2021-12-06 22:24:37 +0200

    написал для себя коментарии

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let array = [];
+let array = []; // память которую скреплю с базой даных
 let x ;
-function  add(x) {
+function  add(x) {  // Функция добавление
     array[array.length] = x;
 };
 let y ;
-function clear(y) {
+function clear(y) { // Удаление определеного значение
     array.splice(y , 1);
 };
-let indicator  = array.length;
-function clear_all() {
+let indicator  = array.length; // Количество закладок
+function clear_all() {  // Удаление всех закладок
     array = [];
 };
-let time;
