429466869623f65deebaf67f22986b3abd119a4d
Pavlo Yerebakan <ppvr3407@gmail.com>
2021-12-07 22:28:00 +0200

    добавление класов

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let array = []; // память которую скреплю с базой даных
+let array = [ 13 , 12, 4324, 'gsdbfdngdb']; // память которую скреплю с базой даных
 let x ;
 function  add(x) {  // Функция добавление
     array[array.length] = x;
@@ -11,3 +11,11 @@ let indicator  = array.length; // Количество закладок
 function clear_all() {  // Удаление всех закладок
     array = [];
 };
+let list  ;
+ for (let i = 0; i < array.length; i++) {
+    list = array[i];
+    var elem = document.createElement("div");
+    elem.className = 'fr';
+    elem.innerHTML = list;
+    document.body.append(elem);
+}
