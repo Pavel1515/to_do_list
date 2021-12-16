cdfd33e3643bc0f406c387a4a00a7b98a6dd83f2
Pavlo Yerebakan <ppvr3407@gmail.com>
2021-12-16 23:16:26 +0200

    ПОКА НЕ ПОЛУЧАЕТСЯ СОЕДЕНИТЬ БЕЗ ПОТЕРИ МАСИВ В ЛОКАЛ СТРЕЙДЖ

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 let array = []; // память которую скреплю с базой даных
-let x ;
-function  add(x) {  // Функция добавление
-    array[array.length] = x;
-};
 let y ;
 function clear(y) { // Удаление определеного значение
     array.splice(y , 1);
@@ -12,9 +8,10 @@ function clear_all() {  // Удаление всех закладок
     array = [];
 };
 let list  ;
+let new_array = JSON.parse(localStorage.getItem("array"));
 function render(){
-    for (let i = 0; i < array.length; i++) {
-        list = array[i];
+     for (let i = 0; i < new_array.length; i++) {
+        list = new_array[i];
         var elem = document.createElement("li");
         elem.className = 'fr';
         elem.innerHTML = list;
@@ -28,9 +25,10 @@ render();
     let input = document.querySelector('#input');
     input.addEventListener('keydown', function(e) {
         if (e.keyCode === 13) {
-         add(input.value);
-         
-          render.reload();
+        let inp = input.value;
+        array[array.length] = inp;
+        localStorage.setItem("array", JSON.stringify(array));
+        
         }
       })
 
